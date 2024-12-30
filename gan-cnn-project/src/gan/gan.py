0f1e18a315a9e65f37709698d6a0b4f144f8f247
Om Patel <om175719@gmail.com>
2024-12-29 22:18:24 -0500

    Re-adding all files

--- gan-cnn-project/src/gan/gan.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.models import Sequential
-
-def build_gan(generator, discriminator):
-    discriminator.trainable = False
-    model = Sequential([generator, discriminator])
-    model.compile(optimizer=tf.keras.optimizers.Adam(0.0002, 0.5),
-                  loss='binary_crossentropy')
-    return model
